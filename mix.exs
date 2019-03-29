4a8a50972535a00f08b910c429cb82af940be9a7
Chris Dosé <chris.dose@gmail.com>
2019-03-29 00:05:17 -0700

    Docs + License + DB in CI

--- mix.exs
+++ mix.exs
@@ -1,17 +1,23 @@
 defmodule EctoDiff.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/peek-travel/ecto_diff"
+
   def project do
     [
       app: :ecto_diff,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: preferred_cli_env(),
-      dialyzer: dialyzer()
+      dialyzer: dialyzer(),
+      docs: docs(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -42,13 +48,42 @@ defmodule EctoDiff.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      main: "EctoDiff",
+      source_ref: @version,
+      source_url: @source_url,
+      extras: ["README.md", "LICENSE.md"]
+    ]
+  end
+
+  defp description do
+    """
+    Calculate the difference between two Ecto structs
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", ".formatter.exs", "mix.exs", "README.md", "LICENSE.md", "CHANGELOG.md"],
+      maintainers: ["Chris Dosé <chris.dose@gmail.com>"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url,
+        "Readme" => "#{@source_url}/blob/#{@version}/README.md",
+        "Changelog" => "#{@source_url}/blob/#{@version}/CHANGELOG.md"
+      }
+    ]
+  end
+
   defp deps do
     [
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:ecto, "~> 3.0"},
-      {:ecto_sql, "~> 3.0", only: [:dev, :test]},
-      {:excoveralls, "~> 0.10", only: :test},
       {:dialyxir, "~> 1.0.0-rc.5", only: [:dev], runtime: false},
+      {:ecto_sql, "~> 3.0", only: [:dev, :test]},
+      {:ecto, "~> 3.0"},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
       {:jason, ">= 1.0.0", only: [:dev, :test]},
       {:postgrex, ">= 0.0.0", only: [:dev, :test]}
     ]
