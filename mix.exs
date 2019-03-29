5e50b6d314718245e8bb3c72e66148b5c76a8b8a
Chris Dosé <chris.dose@gmail.com>
2019-03-28 20:31:58 -0700

    allow Elixir 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EctoDiff.MixProject do
     [
       app: :ecto_diff,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
